f76dcdad1f32dd30cc2c165335f49a9d29dc2bd4
unknown <sylvievrebos@gmail.com>
2018-07-01 10:41:33 +0200

    Checkboxlist + hosts.json

--- app.js
+++ app.js
@@ -1,8 +1,46 @@
-var formApp = angular.module('formApp', [])
+angular
+    .module('App',[])
+    .controller('MainCtrl',function($scope,$http){
 
-    .controller('formController', function($scope) {
-  
-        // we will store our form data in this object
-        $scope.formData = {};
         
+
+        $http.get("hosts.json")
+        .then(function(response) {
+            $scope.hosts = response.data;
+        });
+
+
+        $scope.records = [
+        {"id":1,"name":"Dev"},
+        {"id":2,"name":"Staging"},
+        {"id":3,"name":"Production"},
+        ];
+
+        $scope.selected ={};
+
+        for(x in $scope.records){
+
+            console.log($scope.records[x].id);
+            $scope.selected[$scope.records[x].id]=true;
+
+        }
+
+        $scope.minSelected = function(){
+            var count = 0;
+            for(x in $scope.selected){
+                if($scope.selected[x]) 
+                {
+                    count++;
+                    $scope.item=x;
+                }
+                
+            }
+
+            if(count === 0){
+            	
+              $scope.selected[$scope.item]=true;
+            } 
+            //return (count===3) ? true : false;
+            
+        };
     });
